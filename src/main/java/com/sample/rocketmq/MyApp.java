8e1851dcdda020b71a05ba032f741b92b061a2ff
qlhan <qlhan@incarcloud.com>
2022-06-16 11:46:44 +0800

    0.4.0-SNAPSHOT add stream config

--- src/main/java/com/sample/rocketmq/MyApp.java
+++ src/main/java/com/sample/rocketmq/MyApp.java
@@ -1,7 +1,9 @@
 package com.sample.rocketmq;
 
+import com.sample.rocketmq.msg.MySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 
 /**
  * 学习应用
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Aaric, created on 2022-06-14T14:50.
  * @version 0.1.0-SNAPSHOT
  */
+@EnableBinding(MySource.class)
 @SpringBootApplication
 public class MyApp {
 
--- src/main/java/com/sample/rocketmq/api/conroller/TestController.java
+++ src/main/java/com/sample/rocketmq/api/conroller/TestController.java
@@ -1,7 +1,9 @@
 package com.sample.rocketmq.api.conroller;
 
 import com.sample.rocketmq.api.TestApi;
+import com.sample.rocketmq.msg.MySource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/test")
 public class TestController implements TestApi {
 
+    @Autowired
+    private MySource mySource;
+
     @GetMapping("/hello")
     @Override
     public String helloGet(@RequestParam String name) {
+        System.err.println(mySource);
         return String.format("hello, %s!", name);
     }
 }
